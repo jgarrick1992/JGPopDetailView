f366209bd167cdf3d82eb0770f62a91a72655d90
Ji Fu <ji.fu@icloud.com>
2016-07-31 22:21:37 +0800

    project init.

---
+++ JGPopDetailView/JGPopDetailView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JGPopDetailView
+//
+//  Created by Ji Fu on 7/31/16.
+//  Copyright © 2016 Ji Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JGPopDetailView/JGPopDetailView/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  JGPopDetailView
+//
+//  Created by Ji Fu on 7/31/16.
+//  Copyright © 2016 Ji Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
